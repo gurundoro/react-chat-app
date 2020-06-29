5fa02d00eb39ab6318a6755232016bfbab3b6a19
kudzanayi <kdzvairo@gmail.com>
2020-06-29 08:55:51 -0400

    chat list display

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,11 +1,61 @@
 import React from 'react'
+import ChatListComponent from '../chatlist/chatList'
+const firebase = require("firebase")
 
 class DashboardComponent extends React.Component {
 
+    constructor(){
+        super()
+        this.state = {
+            selectedChat: null,
+            newChatFormVisible: false,
+            email: null,
+            chats: []
+        }
+    }
+
     render(){
         return(
-            <div>Hello World From Dashboard</div>
+            <div>
+            <div>Hello From DashboardComponent</div>
+            <ChatListComponent 
+            history={this.props.history}
+            newChatBtnFn={this.newChatBtnClicked}
+            selectChatFn={this.selectChat} 
+            chat={this.state.chats}
+            email={this.state.email}
+            selectedChatIndex={this.state.selectedChat}
+             />
+            </div>
         ) 
+    }
+
+    selectChat = () => {
+        console.log('Selected a chat')
+    }
+
+    newChatBtnClicked = () => this.setState({newChatFormVisible: true, selectedChat: null})
+
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(async _usr => {
+            if(!_usr)
+                this.props.history.push('/login');
+                else{
+                    await firebase
+                    .firestore()
+                    .collection('chats')
+                    .where('users', 'array-contains', _usr.email)
+                    .onSnapshot(async res => {
+                        const chats = res.docs.map(_doc => _doc.data())
+                        await this.setState({
+                            email: _usr.email,
+                            chats: chats
+                        })
+                        console.log(this.state)
+                    })
+
+                }
+        })
     }
 }
 
--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import FormControl from '@material-ui/core/FormControl'
 import InputLabel from '@material-ui/core/InputLabel'
 import Input from '@material-ui/core/Input'
 import Paper from '@material-ui/core/Paper'
 import withStyles from '@material-ui/core/styles/withStyles'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import Typography from '@material-ui/core/Button'
-import styles from './style'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import styles from './styles'
 const firebase = require("firebase")
 
 class SignupComponent extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            email:null,
+            password: null,
+            passwordConfirmation: null,
+            signupError: ''
+        }
+    }
 
     render(){
 
@@ -25,21 +37,82 @@ class SignupComponent extends Component {
                         Sign Up!
                     </Typography>
                     <form onSubmit={(e) => this.submitSignup(e)} className={classes.form}>
-                    <FormControl required fullWidth margin='normal'></FormControl>
-                    <InputLabel htmlFor='signup-email-input'>Enter Your Email</InputLabel>
-                    <Input autoComplete='email' onChange={e => this.userTyping('email', e)} autoFocus />
+                    <FormControl required fullWidth margin='normal'>
+                      <InputLabel htmlFor='signup-email-input'>Enter Your Email</InputLabel>
+                      <Input autoComplete='email' onChange={e => this.userTyping('email', e)} autoFocus id='signup-email-input'/>
+                    </FormControl>
+                    <FormControl required fullWidth margin='normal'>
+                       <InputLabel htmlFor='signup-password-input'>Create A Password</InputLabel>
+                       <Input type='password' onChange={e => this.userTyping('password', e)} id='signup-password-input' />
+                    </FormControl>
+                    <FormControl required fullWidth margin='normal'>
+                       <InputLabel htmlFor='signup-password-confirmation-input'>Confirm Your Password</InputLabel>
+                       <Input type='password' onChange={e => this.userTyping('passwordConfirmation', e)} id='signup-password-confirmation' />
+                    </FormControl>
+                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>Submit</Button>
                     </form>
+                    {
+                     this.state.signupError ? 
+                     <Typography component='h5' variant='h6' className={classes.errorText}>
+                     {this.state.signupError}
+                     </Typography> :
+                     null
+                    }
+                    <Typography component='h5' variant='h6' className={classes.hasAccountHeader}>Already Have An Account</Typography>
+                    <Link className={classes.logInLink} to='/login'>Log In!</Link>
                 </Paper>
             </main>
             ) 
     }
 
+    formIsValid = () => this.state.password === this.state.passwordConfirmation
+
     userTyping = (type, e) => {
-        console.log(type, e)
+        switch(type){
+            case 'email':
+            this.setState({email: e.target.value})
+            break;
+            case 'password':
+            this.setState({password: e.target.value})
+            break;
+            case 'passwordConfirmation':
+            this.setState({passwordConfirmation: e.target.value})
+            break;
+            default:
+             break;
+        }
     }
 
     submitSignup = e => {
-        console.log('submitting')
+        e.preventDefault()
+
+        if(!this.formIsValid()){
+            this.setState({signupError: 'Passwords Do Not Match'})
+            return 
+        }
+
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(authRes => {
+            const userObj = {
+                email: authRes.user.email
+            }
+            firebase
+            .firestore()
+            .collection('users')
+            .doc(this.state.email)
+            .set(userObj)
+            .then(() => {
+                this.props.history.push('/dashboard')
+            }, dbErr => {
+                console.log(dbErr)
+                this.setState({ signupError: 'failed to add user'})
+            })
+        }, authErr => {
+            console.log(authErr);
+            this.setState({ signupError: 'failed to add user'})
+        })
     }
 }
 
